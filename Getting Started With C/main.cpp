d21206b1452507fd63fbd8b1684ab7b04df721de
Ali Khan <alikhan37544@gmail.com>
2023-08-30 04:29:22 +0530

    .

--- Getting Started With C/main.cpp
+++ Getting Started With C/main.cpp
@@ -5,8 +5,9 @@ int main() {
     std::cout << "Number 1" << std::endl;
     std::cout << "Number 2";
 
-    for (int i=0; i<10; ++i) {
+    for (int i = 0; i < 10; ++i) {
         std::cout << "this is my name 10 times" << std::endl;
     }
+
     return 0;
 }
